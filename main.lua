4877dfb185d392ec1a92dbe726f6502b13bb34b9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-01 17:34:17 -0400

    add more components and comments

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+dofile("ecs.lua")
+
 function love.keypressed(key)
     if key == "escape" then
         print("so long folks!")
@@ -8,17 +10,34 @@ end
 -- button entity component system
 local buttons
 
+--[[
+    the button which will be a different color when selected
+    is a number because it utilizes a pointer jump in the ecs
+    starting point is whatever 1 is
+]]--
+
+local current_selection = 1
+
+
 -- loader function, called on engine load
 function love.load()
 
+    -- initialize the buttons ecs
     buttons = ecs:new()
 
+    -- add required componentes for testing
     buttons:add_components({
         "size_x",
         "size_y",
         "position_x",
-        "position_y"
+        "position_y",
+        "center_x",
+        "center_y"
     })
+
+
+
+
 
 end
 
