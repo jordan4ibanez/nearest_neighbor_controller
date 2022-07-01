563e36be6c30eb1e84c2af284651013437b4c606
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-01 17:28:40 -0400

    add fluff to main

--- main.lua
+++ main.lua
@@ -1,13 +1,22 @@
+function love.keypressed(key)
+    if key == "escape" then
+        print("so long folks!")
+        love.event.quit()
+    end
+end
+
+
+-- loader function, called on engine load
 function love.load()
 
 end
 
-
+-- update loop, the engine loop process
 function love.update(delta)
 
 end
 
+-- draw loop, the engine opengl draw loop
 function love.draw()
-
 
 end
